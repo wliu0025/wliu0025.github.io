655d3a3c97906a73d0059ed66663ddd7ce43cf2b
wei <wliu0025@student.monash.edu>
2023-05-23 14:02:16 +1000

    template fit5147 projects

---
+++ fit5147dvp/js/myscript.js
@@ -0,0 +1,12 @@
+$(function(){
+    $("#startBtn").click(function(){
+        //triger button click in code: .trigger('click')
+        //triger a click in code: 
+        $("#nextPage")[0].click()
+    })
+
+
+    
+
+
+})
